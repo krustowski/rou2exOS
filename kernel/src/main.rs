12f3c58f66027ce6de667238eac0c0aa04c058f1
Tackx <tack@vxn.dev>
2025-04-27 15:18:49 +0200

    Remove unneccessary curly braces

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use core::panic::{PanicInfo};
+use core::panic::PanicInfo;
 
 //mod multiboot2_header;
 

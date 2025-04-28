2a8c29bc71a8f5d0e9334b8b6dc55204b650890f
krusty <krusty@vxn.dev>
2025-04-28 03:15:01 +0200

    Add panic location+file handling (printing)

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -32,9 +32,46 @@ pub extern "C" fn _start() {
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    //let vga_index: &mut isize = &mut 0;
+fn panic(info: &PanicInfo) -> ! {
+    let vga_index: &mut isize = &mut 0;
+
+    vga::screen::clear(vga_index);
+
+    if let Some(location) = info.location() {
+        print_string(vga_index, location.file());
+        print_string(vga_index, ":");
+        print_num(vga_index, location.line());
+        vga::write::newline(vga_index);
+    } else {
+        vga::write::string(vga_index, b"No location", 0xc);
+        vga::write::newline(vga_index);
+    }
 
     loop {}
 }
 
+fn print_string(vga_index: &mut isize, s: &str) {
+    for byte in s.bytes() {
+        vga::write::string(vga_index, &[byte], 0xc);
+    }
+}
+
+fn print_num(vga_index: &mut isize, mut num: u32) {
+    let mut buf = [0u8; 10]; // Max u32 = 10 digits
+    let mut i = buf.len();
+
+    if num == 0 {
+        vga::write::string(vga_index, b"0", 0xc);
+        return;
+    }
+
+    while num > 0 {
+        i -= 1;
+        buf[i] = b'0' + (num % 10) as u8;
+        num /= 10;
+    }
+
+    for b in &buf[i..] {
+        vga::write::string(vga_index, &[*b], 0xc);
+    }
+}

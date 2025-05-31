b1bc2efa29b8ca0f722237e2911806cc210b3d97
krusty <krusty@vxn.dev>
2025-05-31 12:43:44 +0200

    Reorganize the repo (wip)

--- README.md
+++ README.md
@@ -5,12 +5,9 @@ A second iteration of the RoureXOS operating system, rewritten in Rust.
 + [Original RoureXOS (a blog post)](https://krusty.space/projects/rourexos/)
 + [rou2exOS Rusted Edition (a blog post)](https://blog.vxn.dev/rou2exos-rusted-edition)
 
-## How to use (kernel)
+## How to build and run
 
 ```shell
-# go to the `kernel` subdirectory
-cd kernel
-
 # install Rust and its dependencies
 make init
 
@@ -27,5 +24,28 @@ make run_iso_floppy
 # (alternative) run the kernel exclusively only (needs the `bootloader` dependency in Cargo.toml to be added)
 cargo bootimage
 make run
+```
+
+## How to test ICMP/SLIP 
+
+Run the kernel in QEMU to get the `pty` number in stdout:
+
+```
+make run
+
+char device redirected to /dev/pts/3 (label serial0)
+```
+
+Listen for SLIP packets and create a `sl0` interface:
+
+```
+sudo slattach -L -p slip -s 115200 /dev/pts/3
+sudo ifconfig sl0 192.168.3.1 pointopoint 192.168.3.2 up
+```
+
+Catch packets using `tcpdump`:
+
+```
+sudo tcpdump -i sl0
 ```
 
---
+++ src/video/mode.rs
@@ -0,0 +1,83 @@
+enum VideoMode {
+    Framebuffer {
+        address: *mut u8,
+        pitch: usize,
+        width: usize,
+        height: usize,
+        bpp: u8,
+    },
+    TextMode,
+}
+
+static mut VIDEO_MODE: Option<VideoMode> = Some(VideoMode::TextMode);
+
+pub fn init_video(fb: &crate::init::boot::FramebufferTag) {
+    unsafe {
+    VIDEO_MODE = Some(VideoMode::Framebuffer {
+        address: fb.addr as *mut u8,
+        pitch: fb.pitch as usize,
+        width: fb.width as usize,
+        height: fb.height as usize,
+        bpp: fb.bpp,
+    });
+    }
+}
+
+pub fn put_pixel(x: usize, y: usize, r: u8, g: u8, b: u8) {
+    unsafe {
+        match VIDEO_MODE {
+            Some(VideoMode::Framebuffer {
+                address,
+                pitch,
+                width,
+                height,
+                bpp,
+            }) => {
+                if x >= width || y >= height {
+                    return;
+                }
+
+                let offset = y * pitch + x * (bpp as usize / 8);
+                let ptr = address.add(offset);
+
+                match bpp {
+                    32 => {
+                        let color = (r as u32) << 16 | (g as u32) << 8 | (b as u32);
+                        *(ptr as *mut u32) = color;
+                    }
+                    16 => {
+                        let r5 = (r >> 3) as u16;
+                        let g6 = (g >> 2) as u16;
+                        let b5 = (b >> 3) as u16;
+                        let color = (r5 << 11) | (g6 << 5) | b5;
+                        *(ptr as *mut u16) = color;
+                    }
+                    _ => {} // Unsupported
+                }
+            }
+            Some(VideoMode::TextMode) => {
+                // no-op for pixel drawing in text mode
+            }
+            _ => {}
+        }
+    }
+}
+
+const VGA_BUFFER: *mut u8 = 0xb8000 as *mut u8;
+const VGA_WIDTH: usize = 80;
+
+pub fn put_char(x: usize, y: usize, ch: u8, color: u8) {
+    unsafe {
+        match VIDEO_MODE {
+            Some(VideoMode::TextMode) => {
+                let offset = 2 * (y * VGA_WIDTH + x);
+                *VGA_BUFFER.add(offset) = ch;
+                *VGA_BUFFER.add(offset + 1) = color;
+            }
+            _ => {
+                // Optional: render bitmap font to framebuffer instead
+            }
+        }
+    }
+}
+

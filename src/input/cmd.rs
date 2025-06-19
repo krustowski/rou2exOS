9c57b112b1b0e6963c5451816f71b4e8e05081b0
krusty <krusty@vxn.dev>
2025-06-19 14:53:14 +0200

    Refactor and tidy up, v0.7.5

--- src/input/cmd.rs
+++ src/input/cmd.rs
@@ -12,7 +12,7 @@ use crate::vga::write::newline;
 use crate::input::keyboard;
 use crate::tui::{widget::{Container, Window, Label}, app::TuiApp};
 
-const KERNEL_VERSION: &[u8] = b"0.7.4";
+const KERNEL_VERSION: &[u8] = b"0.7.5";
 
 struct Command {
     name: &'static [u8],

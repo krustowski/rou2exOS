9838648396a4c8eb9132c5a0b633dcd74b3acfcd
krusty <krusty@vxn.dev>
2025-06-21 04:33:23 +0200

    Update print macros

--- src/macros.rs
+++ src/macros.rs
@@ -2,7 +2,10 @@ use spin::{mutex::Mutex};
 use core::sync::atomic::{AtomicBool, Ordering};
 use crate::vga::writer::{Writer};
 
+/// Wrapped Writer instance guarded by Mutex.
 pub static mut WRITER: Option<Mutex<Writer>> = None;
+
+/// Helper static boolean to ensure that the global Writer instance is created just once.
 static WRITER_INIT: AtomicBool = AtomicBool::new(false);
 
 /// Initializes the global Writer instance
@@ -15,28 +18,44 @@ pub fn init_writer() {
     }
 }
 
-/// Returns a wrapped Writer instance guarded by Mutex in Option
+/// Returns a wrapped Writer instance guarded by Mutex in Option. Beware that this invocation locks
+/// the Writer instance and all print macros therefore can fail silently.
 pub fn get_writer() -> Option<spin::MutexGuard<'static, Writer>> {
     if WRITER_INIT.load(Ordering::Relaxed) {
         unsafe { WRITER.as_ref().map(|m| m.lock()) }
     } else {
+        // Not initialized yet
         None
     }
 }
 
+//
+//  PRINT MACROS
+//
+
+/// Macro to render all rows with the currently set ColorCode.
+#[macro_export]
+macro_rules! clear_screen {
+    () => {
+        if let Some(mut writer) = $crate::macros::get_writer() {
+            writer.clear_screen();
+        }
+    };
+}
+
+/// Prints the error string to screen in red.
 #[macro_export]
 macro_rules! error {
     () => {
         $crate::print!("\n");
     };
     ($arg:expr $(,)?) => {
-        use crate::vga::writer::Color;
-
         // Set yellow chars on black
-        $crate::print!($arg, Color::Red, Color::Black);
+        $crate::print!($arg, $crate::vga::writer::Color::Red, $crate::vga::writer::Color::Black);
     };
 }
 
+/// Prints the warning string to screen in yellow.
 #[macro_export]
 macro_rules! warn {
     () => {
@@ -53,7 +72,7 @@ macro_rules! warn {
 macro_rules! printb {
     ($arg:expr) => {
         if let Some(mut writer) = $crate::macros::get_writer() { 
-            writer.set_color(Color::White, Color::Black);
+            writer.set_color($crate::vga::writer::Color::White, $crate::vga::writer::Color::Black);
             for b in $arg {
                 writer.write_byte(*b);
             }
@@ -61,7 +80,7 @@ macro_rules! printb {
     };
 }
 
-
+/// Meta macro to include the newline implicitly at the end of provided string.
 #[macro_export]
 macro_rules! println {
     () => ($crate::print!("\n"));
@@ -71,19 +90,20 @@ macro_rules! println {
     });
 }
 
+/// The main string printing macro. Takes in 1-3 arguments. The first one is always the string to
+/// print, and the latter ones are to change the foreground color (and background respectively) of
+/// the characters printed to screen.
 #[macro_export]
 macro_rules! print {
     ($arg:expr) => {
         if let Some(mut writer) = $crate::macros::get_writer() { 
-            writer.set_color(Color::White, Color::Black);
+            writer.set_color($crate::vga::writer::Color::White, $crate::vga::writer::Color::Black);
             writer.write_str_raw($arg);
         }
     };
     ($arg:expr, $fg:expr) => {
-        use crate::vga::writer::Color;
-
         if let Some(mut writer) = $crate::macros::get_writer() { 
-            writer.set_color($fg, Color::Black);
+            writer.set_color($fg, $crate::vga::writer::Color::Black);
             writer.write_str_raw($arg);
         }
     };

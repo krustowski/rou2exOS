319ce28e039068c68f8818334bbf0e2bdedf9e56
krusty <krusty@vxn.dev>
2025-06-24 16:24:50 +0200

    Test that framebuffer is writtable, tidy up

--- src/init/boot.rs
+++ src/init/boot.rs
@@ -1,17 +1,12 @@
-use crate::vga::{
-    write::{string, newline, number},
-    buffer::Color,
-};
-use super::result::InitResult;
+use x86_64::VirtAddr;
+
+use crate::{debug::dump_debug_log_to_file, init::{config::{p1_fb_table, p1_fb_table_2, p2_fb_table, p3_fb_table, p4_table}, }, vga::{
+    buffer::Color, write::{newline, number, string}
+} };
+use super::{result::InitResult};
 
 pub fn print_info(vga_index: &mut isize, multiboot_ptr: u64) -> InitResult {
     unsafe {
-        /*parse_multiboot2_info(multiboot_ptr as usize, |msg| {
-          for b in msg.as_bytes() {
-          vga::write::byte(vga_index, *b, vga::buffer::Color::Yellow);
-          }
-          });*/
-
         debug!("Multiboot2 pointer: ");
         debugn!(multiboot_ptr);
         debugln!("");
@@ -30,9 +25,9 @@ pub fn print_info(vga_index: &mut isize, multiboot_ptr: u64) -> InitResult {
 
 #[repr(C)]
 #[derive(Debug)]
-struct TagHeader {
-    typ: u32,
-    size: u32,
+pub struct TagHeader {
+    pub typ: u32,
+    pub size: u32,
 }
 
 #[repr(C)]
@@ -42,7 +37,7 @@ struct MemoryMapTag {
     size: u32,          // size of this tag including entries
     entry_size: u32,    // size of each entry (usually 24 bytes)
     entry_version: u32, // usually 0
-                        // followed by [MemoryMapEntry]...
+                        
 }
 
 #[repr(C, packed)]
@@ -54,6 +49,7 @@ struct MemoryMapEntry {
     reserved: u32,  // must be 0
 }
 
+#[derive(Clone,Copy)]
 #[repr(C, packed)]
 pub struct FramebufferTag {
     typ: u32,
@@ -65,10 +61,8 @@ pub struct FramebufferTag {
     pub bpp: u8,
     fb_type: u8,
     reserved: u16,
-    // followed by palette or color info
 }
 
-//pub unsafe fn parse_multiboot2_info(base_addr: usize, mut log_fn: impl FnMut(&str)) {
 pub unsafe fn parse_multiboot2_info(vga_index: &mut isize, base_addr: usize) -> usize {
     // Ensure alignment (Multiboot2 requires 8-byte aligned structure)
     let addr = align_up(base_addr, 8);
@@ -116,7 +110,7 @@ pub unsafe fn parse_multiboot2_info(vga_index: &mut isize, base_addr: usize) -> 
                 debugln!(cmdline);
             }
             6 => {
-                //debug!("Memory map tag");
+                debugln!("Memory map tag");
 
                 let mmap_tag = &*(ptr as *const MemoryMapTag);
                 let entries_start = (addr + core::mem::size_of::<MemoryMapTag>()) as *const u8;
@@ -132,19 +126,21 @@ pub unsafe fn parse_multiboot2_info(vga_index: &mut isize, base_addr: usize) -> 
                         if entry.typ == 1 {
                             debug!("Usable memory region: ");
                             debugn!(entry.base_addr as u64);
-                            debug!("-");
+                            debug!(" - ");
                             debugn!(entry.length as u64);
-                            debugln!("");
-
-                            continue;
+                            debugln!(" B");
                         }
                     }
                 }
             }
             8 => {
-                debug!("Framebuffer tag: ");
+                debugln!("Framebuffer tag: ");
 
                 let fb_tag = &*(ptr as *const FramebufferTag);
+
+                debug!("Framebuffer address: ");
+                debugn!(fb_tag.addr as u64);
+                debugln!("");
 
                 debug!("(bpp + res): ");
                 debugn!(fb_tag.bpp as u64);
@@ -153,10 +149,56 @@ pub unsafe fn parse_multiboot2_info(vga_index: &mut isize, base_addr: usize) -> 
                 debug!("x");
                 debugn!(fb_tag.height as u64);
                 debugln!("");
+
+                use core::ptr;
+
+                unsafe {
+                    let fb_ptr = 0xFFFF_FF80_0000_0000 as *mut u32;
+
+                    /*super::video::map_framebuffer(
+                        fb_tag.addr,                       
+                        0xFFFF_FF80_0000_0000,               
+                        8 * 1024 * 1024,                  
+                        &mut P4_TABLE,
+                        &mut P3_FB,
+                        &mut [&mut P2_FB_0, &mut P2_FB_1],
+                        &mut [
+                        &mut P1_FB_0, &mut P1_FB_1,
+                        &mut P1_FB_2, &mut P1_FB_3,
+                        &mut P1_FB_4, &mut P1_FB_5,
+                        &mut P1_FB_6, &mut P1_FB_7,
+                        &mut P1_FB_8, &mut P1_FB_9,
+                        &mut P1_FB_10, &mut P1_FB_11,
+                        &mut P1_FB_12, &mut P1_FB_13,
+                        &mut P1_FB_14, &mut P1_FB_15,
+                        ],
+                    );*/
+
+                    x86_64::instructions::tlb::flush_all();
+
+                    let fb_size = (fb_tag.pitch * fb_tag.height) as usize;
+                    //let fb = unsafe { core::slice::from_raw_parts_mut(fb_ptr, fb_size) };
+
+                    for y in 0..fb_tag.height  {
+                      for x in 0..fb_tag.width  {
+                      let offset = y * fb_tag.pitch + x * (fb_tag.bpp as u32 / 8);
+                      let pixel = fb_ptr.add(offset as usize);
+
+                      ptr::write(pixel, 0x00);                    
+                      ptr::write(pixel.add(1), 0xFF);      
+                      ptr::write(pixel.add(2), 0x00);      
+                      ptr::write(pixel.add(3), 0xFF);      
+                      }
+                   }
+                }
+
+                dump_debug_log_to_file(&mut 0);
+
             }
             _ => {
-                debugln!("Unknown tag");
-                //log_fn(&format!("  Unknown tag: type={}, size={}", tag.typ, tag.size));
+                debug!("Unknown tag: ");
+                debugn!(tag.typ);
+                debugln!("");
             }
         }
 
@@ -174,4 +216,25 @@ pub unsafe fn parse_multiboot2_info(vga_index: &mut isize, base_addr: usize) -> 
 fn align_up(x: usize, align: usize) -> usize {
     (x + align - 1) & !(align - 1)
 }
+
+static mut P4_TABLE: [u64; 512] = [0; 512];
+static mut P3_FB: [u64; 512] = [0; 512];
+static mut P2_FB_0: [u64; 512] = [0; 512];
+static mut P2_FB_1: [u64; 512] = [0; 512];
+static mut P1_FB_0: [u64; 512] = [0; 512];
+static mut P1_FB_1: [u64; 512] = [0; 512];
+static mut P1_FB_2: [u64; 512] = [0; 512];
+static mut P1_FB_3: [u64; 512] = [0; 512];
+static mut P1_FB_4: [u64; 512] = [0; 512];
+static mut P1_FB_5: [u64; 512] = [0; 512];
+static mut P1_FB_6: [u64; 512] = [0; 512];
+static mut P1_FB_7: [u64; 512] = [0; 512];
+static mut P1_FB_8: [u64; 512] = [0; 512];
+static mut P1_FB_9: [u64; 512] = [0; 512];
+static mut P1_FB_10: [u64; 512] = [0; 512];
+static mut P1_FB_11: [u64; 512] = [0; 512];
+static mut P1_FB_12: [u64; 512] = [0; 512];
+static mut P1_FB_13: [u64; 512] = [0; 512];
+static mut P1_FB_14: [u64; 512] = [0; 512];
+static mut P1_FB_15: [u64; 512] = [0; 512];
 

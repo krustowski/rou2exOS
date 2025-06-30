cfe2b64a9816e4b726e22f4272e85de15a4e3860
krusty <krusty@vxn.dev>
2025-06-30 13:00:10 +0200

    Add expanded syscall handler (wip)

--- src/api/int_7f.asm
+++ src/api/int_7f.asm
@@ -1,20 +1,31 @@
+BITS 64
+
 global int7f_isr
 extern syscall_handler
 
 section .text
 int7f_isr:
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rdi
+    ; Test the invocation: print '!'
+    ;mov byte [0xb8000], '!'
+    ;mov byte [0xb8001], 0x1F
+
+    ;push rax
+    ;push rdi
+    ;push rsi
+    ;push rdx
+    ;push rcx
+    ;push r8
+    ;push r9
+
     call syscall_handler
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
+
+    ;pop r9
+    ;pop r8
+    ;pop rcx
+    ;pop rdx
+    ;pop rsi
+    ;pop rdi
+    ;pop rax
+
     iretq
 
--- src/api/syscall.rs
+++ src/api/syscall.rs
@@ -1,25 +1,48 @@
-/// This function is the syscall dispatching routine. It is called exclusively from the ISR for
-/// interrupt 0x7f. 
+/// This function is the syscall ABI dispatching routine. It is called exclusively from the ISR 
+/// for interrupt 0x7f. 
 #[no_mangle]
 pub extern "C" fn syscall_handler() {
-    let syscall_number: u32;
+    let (syscall_no, arg1, arg2): (u64, u64, u64);
 
     unsafe {
         core::arch::asm!(
-            "mov {0:e}, eax",
-            out(reg) syscall_number,
+            "mov {0}, rax",
+            "mov {1}, rdi",
+            "mov {2}, rsi",
+            out(reg) syscall_no,
+            out(reg) arg1,
+            out(reg) arg2,
         );
     }
 
-    match syscall_number {
+    match syscall_no {
         1 => {
-            debug!("Syscall 1 called!\n");
+            debug!("Syscall 01 called!\n");
+
+            let ptr = arg1 as *const u8;
+            let len = arg2 as usize;
+            let slice = unsafe { core::slice::from_raw_parts(ptr, len) };
+
+            printb!(slice);
+            println!("");
         }
         _ => {
             debug!("Unknown syscall: ");
-            debugn!(syscall_number);
+            debugn!(syscall_no);
+            debug!(", arg1: ");
+            debugn!(arg1);
+            debug!(", arg2: ");
+            debugn!(arg2);
             debugln!("");
         }
+    }
+
+    unsafe {
+        core::arch::asm!(
+            //"mov rax, {0}",
+            "mov rax, {0}",
+            in(reg) 42u64,
+        );
     }
 }
 

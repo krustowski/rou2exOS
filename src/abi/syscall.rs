b7eab26959ca168a89feb3518f70d83e5dea3283
krusty <krusty@vxn.dev>
2025-08-04 23:33:12 +0200

    Increment version: v0.9.3

--- src/abi/syscall.rs
+++ src/abi/syscall.rs
@@ -93,7 +93,7 @@ pub extern "C" fn syscall_handler() {
                     unsafe {
                         let name = b"rou2ex";
                         let user = b"guest";
-                        let version = b"v0.9.2";
+                        let version = b"v0.9.3";
                         let path = b"/";
 
                         if let Some(nm) = (*sysinfo_ptr).system_name.get_mut(0..name.len()) {
--- src/input/cmd.rs
+++ src/input/cmd.rs
@@ -16,7 +16,7 @@ use crate::video;
 use crate::input::keyboard;
 use crate::tui::{widget::{Container, Window, Label}, app::TuiApp};
 
-const KERNEL_VERSION: &[u8] = b"0.9.2";
+const KERNEL_VERSION: &[u8] = b"0.9.3";
 
 struct Command {
     name: &'static [u8],
